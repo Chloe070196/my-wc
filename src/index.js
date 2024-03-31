52e7b097873d5a38b0a43dc3edf141a6c9d2fcd6
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-31 13:25:53 +0100

    feat: wc function runs on command

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const {
+  getFileName,
+  getFileContentStr,
+  getNestedWordArr,
+  getWordCount,
+  getLineCount,
+  getFileSizeInBytes
+} = require("./utils.js");
+
+function wc() {
+  // access file and file content
+  const fileName = getFileName();
+  const fileContentStr = getFileContentStr(fileName);
+  // get the data
+  if (!fileName) {
+    throw Error("file not found");
+  }
+  if (!fileContentStr) {
+    throw Error("empty file");
+  }
+  const lineCount = getLineCount(fileContentStr);
+  const wordCount = getWordCount(fileContentStr);
+  const fileSizeInBytes = getFileSizeInBytes(fileName);
+  // print to the console
+  console.log(
+    lineCount + " ",
+    wordCount + " ",
+    fileSizeInBytes + " ",
+    fileName
+  );
+}
+
+wc();
+
+module.exports = {
+  wc,
+  getFileName,
+  getFileContentStr,
+  getLineCount,
+  getWordCount,
+  getNestedWordArr,
+};
