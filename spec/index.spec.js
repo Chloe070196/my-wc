2694717046f7c52953141e1315ff934ce6d70f11
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-03 12:58:32 +0100

    test: getTotal

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -5,6 +5,7 @@ const {
   getWordCount,
   getNestedWordArr,
   getFileSizeInBytes,
+  getTotal,
 } = require("../lib/utils");
 
 // notes: only unit test are included here
@@ -18,46 +19,62 @@ describe("wc: ", () => {
       expect(result).toEqual(true);
     });
     it("throws an error if either inputs are invalid", () => {
-      expect(() => {checkInputValidity("", "some very long string")}).toThrow(Error("file not found"))
-      expect(() => {checkInputValidity(undefined, "some very long string")}).toThrow(Error("file not found"))
-      expect(() => {checkInputValidity("aFileName", null)}).toThrow(Error("empty file"))
+      expect(() => {
+        checkInputValidity("", "some very long string");
+      }).toThrow(Error("file not found"));
+      expect(() => {
+        checkInputValidity(undefined, "some very long string");
+      }).toThrow(Error("file not found"));
+      expect(() => {
+        checkInputValidity("aFileName", null);
+      }).toThrow(Error("empty file"));
     });
-  })
-  
-  it("getFileContentStr returns the file contents as a string", () => {
-    const result = getFileContentStr("pg132.txt");
-    expect(result).not.toBeUndefined();
-    expect(typeof result).toEqual("string");
-    expect(result.length).toEqual(339294);
   });
 
-  beforeEach(() => {
-    file = getFileContentStr("pg132.txt");
+  describe("when getting the data on individual file: ", () => {
+    it("getFileContentStr returns the file contents as a string", () => {
+      const result = getFileContentStr("pg132.txt");
+      expect(result).not.toBeUndefined();
+      expect(typeof result).toEqual("string");
+      expect(result.length).toEqual(339294);
+    });
+
+    beforeEach(() => {
+      file = getFileContentStr("pg132.txt");
+    });
+
+    it("getNestedWordArr returns nested arrays containing individual words", () => {
+      const result = getNestedWordArr(file);
+      expect(result).not.toBeUndefined();
+      expect(typeof result).toEqual("object");
+      expect(typeof result[0]).toEqual("object");
+      expect(typeof result[0][0]).not.toBeUndefined();
+    });
+    it("getLineCount returns the number of lines", () => {
+      const result = getLineCount(file);
+      expect(result).not.toBeUndefined();
+      expect(typeof result).toEqual("number");
+      expect(result).toEqual(7147);
+    });
+    it("getWordCount returns the number of words", () => {
+      const result = getWordCount(file);
+      expect(result).not.toBeUndefined();
+      expect(typeof result).toEqual("number");
+      expect(result).toEqual(58148);
+    });
+    it("returns the number of bytes of a file", () => {
+      const result = getFileSizeInBytes("pg132.txt");
+      expect(result).not.toBeUndefined();
+      expect(typeof result).toEqual("number");
+      expect(result).toEqual(342192);
+    });
   });
 
-  it("getNestedWordArr returns nested arrays containing individual words", () => {
-    const result = getNestedWordArr(file);
-    expect(result).not.toBeUndefined();
-    expect(typeof result).toEqual("object");
-    expect(typeof result[0]).toEqual("object");
-    expect(typeof result[0][0]).not.toBeUndefined();
-  });
-  it("getLineCount returns the number of lines", () => {
-    const result = getLineCount(file);
-    expect(result).not.toBeUndefined();
-    expect(typeof result).toEqual("number");
-    expect(result).toEqual(7147);
-  });
-  it("getWordCount returns the number of words", () => {
-    const result = getWordCount(file);
-    expect(result).not.toBeUndefined();
-    expect(typeof result).toEqual("number");
-    expect(result).toEqual(58148);
-  });
-  it("returns the number of bytes of a file", () => {
-    const result = getFileSizeInBytes("pg132.txt");
-    expect(result).not.toBeUndefined();
-    expect(typeof result).toEqual("number");
-    expect(result).toEqual(342192);
+  describe("when getting data on multiple files, ", () => {
+    it("returns the sum of the numbers stored in the array passed to it", () => {
+      const result = getTotal([12546, 89767, 8746]);
+      expect(result).not.toBeUndefined();
+      expect(result).toEqual(111059);
+    });
   });
 });
